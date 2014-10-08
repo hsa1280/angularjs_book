f6255dbd097fd4a0daf42474828448551abbfec5
SHIAN HUANG <shian.huang@ftlinc.com>
2014-10-08 17:00:17 -0500

    chapter 18: added files

--- chapter18/directives.js
+++ chapter18/directives.js
@@ -1,15 +1,14 @@
-angular.module("customerDirectives", [])
-	.directive("triButton", function() {
-		return {
-			scope: {counter: "=counter"},
-			link: function(scope, element, attrs) {
-				element.on("click", function(event) {
-					console.log("Button click: " + event.target.innerText);
-
-					scope.$apply( function() {
-						scope.counter++;
-					});
-				});
-			}
-		}
-	});
+angular.module("customDirectives", [])
+.directive("triButton", function () {
+    return {
+        scope: { counter: "=counter" },
+        link: function (scope, element, attrs) {
+            element.on("click", function (event) {
+                console.log("Button click: " + event.target.innerText);
+                scope.$apply(function () {
+                    scope.counter++;
+                });
+            });
+        }
+    }
+});
