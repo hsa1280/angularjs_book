64c0706836c1bdd438f220ab19b3e03e0420d069
SHIAN HUANG <shian.huang@ftlinc.com>
2014-10-24 13:10:44 -0500

    sportsStore: finished up the app. It is working fine.

---
+++ sportsstore/controllers/adminProductController.js
@@ -0,0 +1,50 @@
+angular.module("sportsStoreAdmin")
+.constant("productUrl", "http://localhost:5500/products/")
+.config( function( $httpProvider ) {
+
+	$httpProvider.defaults.withCredentials = true;
+})
+.controller("productCtrl", function( $scope, $resource, productUrl ) {
+
+	$scope.productsResource = $resource( productUrl + ":id", { id: "@id"});
+
+	$scope.listProducts = function() {
+
+		$scope.products = $scope.productsResource.query();
+	}
+
+	$scope.deleteProduct = function( product ) {
+
+		product.$delete().then( function() {
+
+			$scope.products.splice( $scope.products.indexOf( product ), 1);
+		});
+	}
+
+	$scope.createProduct = function( product ) {
+
+		new $scope.productsResource( product ).$save().then( function( newProduct) {
+
+			$scope.products.push( newProduct );
+			$scope.editeProduct = null;
+		});
+	}
+
+	$scope.updateProduct = function( product ) {
+
+		product.$save();
+		$scope.editeProduct = null;
+	}
+
+	$scope.startEdit = function( product ) {
+
+		$scope.editeProduct = product;
+	}
+
+	$scope.cancelEdit = function() {
+		
+		$scope.editeProduct = null;
+	}
+
+	$scope.listProducts();
+});
